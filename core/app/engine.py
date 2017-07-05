8daed401efcb1eb6329e5ca12b4871babb7b0e12
John Snow <johnsnow@nomail.net>
2017-07-05 23:47:50 +0200

    added message buffer so that there is less processing in the message getting

--- core/app/engine.py
+++ core/app/engine.py
@@ -5,42 +5,25 @@ import time
 import json
 import sys
 
-'''
-Server
-'''
 class Engine():
     def __init__(self):
 
-        self.sock = 0
-        self.connections = []
-        self.udata = {}
+        self.sock = 0 #socket
+        self.connections = [] #list of ip's
+        self.udata = {} #usr data
+        self.buffer = {} #mesage buffer
 
     def createServer(self, ip, port, socktype = 'udp'):
-
-        if socktype == 'udp':
-
-            self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-            self.sock.bind((socket.gethostname(), port))
-
-            return 1
-
-        else:
-            print("tcp not available yet")
-            return 0
+        #try-except?
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.sock.bind((socket.gethostname(), port))
+        return 1
 
     def get(self):
-
         data, addr = self.sock.recvfrom(1024)
-        data = data.decode("utf-8")
-        if data[0] == '/':
-            self.switch(data, addr)
-            #if returned data, addr ?? - display action message
-            return None, addr
         return data, addr
 
-    '''
-    sending bytes to single connections
-    '''
+    #sends to onc client
     def post(self, data, client):
         '''
         data is the data to be sent ofc
@@ -48,9 +31,7 @@ class Engine():
         '''
         self.sock.sendto(data.encode("utf-8"), client)
 
-    '''
-    broadcasting - sending bytes to all connections
-    '''
+    #broadcast the message
     def broadcast(self, data):
         if data:
             for conn in self.connections:
@@ -59,20 +40,49 @@ class Engine():
         else:
             return 0
 
-    '''
-    keeping track of active connnections
-    '''
-    def onconnect(self, new):
-        if new not in self.connections:
-            self.connections.append(new)
-            uid = new[0] + ':' + str(new[1]) #special ID, if user cant be accessed delete u data also
+    #saves idetificators from incomming connections
+    #that way it's possible to keep track of some `user list`
+    def onconnect(self, address):
+        if address not in self.connections:
+            self.connections.append(address)
+            uid = address[0] + ':' + str(address[1]) #special ID, if user cant be accessed delete u data also
             entry = {}
-            entry['port'] = new[1]
+            #entry['port'] = address[1] #?
             entry['time'] = time.time()
             self.udata[uid] = entry
-    '''
-    active connections watcher
-    '''
+
+    #incomming messages go into a special buffer
+    #the processing of each message comes later in order
+    #as the messages appear in the buffer
+    def msgbuffer(self, address, data):
+        length = len(self.buffer)
+        tmp = {
+            'data': data,
+            'addr': address
+        }
+        #len should return real length +1 or 0
+        self.buffer[length] = tmp
+        tmp = None
+
+    #check the dictionary of messages in the buffer
+    # process them and also sends each messages(broadcast)
+    #afterwards a messages is deleted from the buffer
+    def bufferProc(self):
+        while 1:
+            if self.buffer:
+                for i in range(0, len(self.buffer)):
+                    self.onconnect(self.buffer[i]['addr'])
+                    #since the server can get 'special command'
+                    #here we will handle them
+                    self.buffer[i]['data'] = self.buffer[i]['data'].decode("utf-8")
+                    if self.buffer[i]['data'][0] == '/':
+                        self.switch(self.buffer[i]['data'],self.buffer[i]['addr'])
+                        self.buffer[i]['data'] = None
+                    #broadcast should not send None type, so yeah
+                    self.broadcast(self.buffer[i]['data'])
+                    del self.buffer[i]
+
+    #probabbly just updates the 'last-seen' time of a client
     def ping(self, client):
         uid = client[0] + ':' + str(client[1])
         newtime = time.time()
@@ -82,9 +92,12 @@ class Engine():
         except KeyError:
             pass
 
+    #to keep up...
     def pong(self, remote):
         self.post('1', remote)
 
+    #it just checks the current user list
+    #probabbly shit implementation
     def clientCheck(self):
         while 1:
             for c in self.connections:
@@ -95,19 +108,23 @@ class Engine():
                     self.post('/ping', c)
                     wait = time.time()
                     while 1:
+                        #has the time changed? break.
                         if cltime != self.udata[uid]['time']:
                             break
+                        #too long? break.
                         if time.time() - float(wait) >= 30:
                             break
+                    #time still the same? this client is not here :/
                     if float(cltime) == self.udata[uid]['time']:
                         #send bye message
                         self.post("/bye", c)
                         self.connections.remove(c)
                         del self.udata[uid]
                         print("removed " + str(c))
-
         time.sleep(30)
 
+    #so that the server can process addational commands
+    #side note: the actual command should come in data param as the message does??
     def switch(self, do, arg):
         options = {
             "pong" : self.pong,
--- udpserver.py
+++ udpserver.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+ #!/usr/bin/python3
 import threading
 from core.app.engine import Engine
 '''
@@ -9,24 +9,12 @@ class Chat():
     def __init__(self):
         self.Server = Engine()
         self.Server.createServer('', 8080, 'udp')
-        print(self.Server)
+        threading.Thread(target=self.Server.bufferProc).start()
         threading.Thread(target=self.Server.clientCheck).start()
         self.loop()
 
-
     def loop(self):
         while 1:
-            data, addr  = self.Server.get()
-            print(addr)
-            self.Server.onconnect(addr)
-            print("adress is --> " + addr[0] + " || " + str(addr[1]))
-            self.Server.broadcast(data)
-            '''
-            for conn in self.Server.connections:
-                print(conn)
-
-            print("_______________________")
-            '''
-            print(self.Server.udata)
-            #print(data)
+            d, a = self.Server.get()
+            self.Server.msgbuffer(a,d)
 cl = Chat()
